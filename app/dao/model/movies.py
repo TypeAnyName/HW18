89822fd595a67cfb93b839f364ec227a4b6aa4f4
TypeAnyName <alexgox11@gmail.com>
2022-05-27 18:21:03 +0300

    рефактор в добавление app и модель для муви

---
+++ app/dao/model/movies.py
@@ -0,0 +1,23 @@
+from app.setup_db import db
+from marshmallow import Schema, fields
+
+
+class Movies(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer)
+    director_id = db.Column(db.Integer)
+
+
+class MoviesSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
